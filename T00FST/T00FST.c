d955827b50cdeeeb22361f9c927a69c80457e6c8
Vladimir V. Gololobov (VG6) <blakexxxd@gmail.com>
2018-06-02 16:05:34 +0400

    MessageBox

---
+++ T00FST/T00FST.c
@@ -0,0 +1,9 @@
+/* Gololobov Vladimir, 10-6, 01.06.2018 */
+#include <windows.h>
+#include <stdio.h>
+
+void main( void )
+{
+  printf("Hello");
+  MessageBox(NULL, "Good morning", "First of June", MB_OK | MB_ICONINFORMATION);
+}
